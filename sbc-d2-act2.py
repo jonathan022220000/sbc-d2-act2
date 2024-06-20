eae38a08430d50ff3f3a54b2ee9ec6ebc73679a4
jonathan022220000 <jonathangenegabuas01@gmail.com>
2024-06-19 19:57:38 -0700

    sbc-d2-act2.py

--- sbc-d2-act2.py
+++ sbc-d2-act2.py
@@ -1,32 +1,24 @@
 from random import randint
 
-ako = input(" hayang to  1 kulob: ")
+ako = input(" '0' hayang to  '1' kulob: ")
+ako = int(ako)
+
 C2 = randint(0,1)
 C3 = randint(0,1)
 
-hayang = 0
-kulob = 1
 
-if ako == kulob:
-    print("Ako:kulob")
-elif ako == hayang:
-    print("Ako:hayang")
-if C2 == 1:
-    print("C2:kulob")
-elif C2 == 0:
-    print("C2:hayang")
-if C3 == 1:
-    print("C3:kulob")
-elif C3 == 0:
-    print("C3:hayang")
-    
+choice = {0: "hayang", 1: "kulob"}
+print(f"ako: {choice [ako]}")
+print(f"C2: {choice [C2]}")
+print(f"C3: {choice [C3]}")
 
 
-if ako == 0 and C2 == 1 and C3 == 1 or ako == 1 and C2 == 0 and C3 == 0 :
+
+if (ako == 0 and C2 == 1 and C3 == 1) or (ako == 1 and C2 == 0 and C3 == 0):
     print("you win")
-elif ako == 1 and C2 == 0 and C3 == 1 or ako == 0 and C2 == 1 and C3 == 0:
+elif (ako == 1 and C2 == 0 and C3 == 1) or (ako == 0 and C2 == 1 and C3 == 0):
     print("C2 win")
-elif ako == 1 and C2 == 1 and C3 == 0 or ako == 0 and C2 == 0 and C3 == 1:
-    print("C2 win")
+elif (ako == 1 and C2 == 1 and C3 == 0) or (ako == 0 and C2 == 0 and C3 == 1):
+    print("C3 win")
 else:
-    print("draw")
+    print("draw1")
